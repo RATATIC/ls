6e945698336c995a172e2f154708f3ce963bb59f
Renat <kagal@itspartner.net>
2021-09-21 18:14:14 +0300

    feature 2: save file name and size

--- head.h
+++ head.h
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <dirent.h>
-
+#include <sys/stat.h>
 
 //Functoin return list of files and folders
-char** open_directory(char*);
+char** open_directory(char*, size_t*);
+
+char* name_file_or_dir (char* , char* );
+
+void free_memory(char** , int);
--- main.c
+++ main.c
@@ -13,43 +13,50 @@ int main(int argc, char** argv){
 	size_t size = 0;
 	
 	getline(&path_dir_with_0, &size, stdin);
+	puts("\n");
 	
 	char path_dir[strlen(path_dir_with_0) - 1];
 	
 	strncpy(path_dir, path_dir_with_0, strlen(path_dir_with_0) - 1);
 	
 	char** catalog;
+	size_t catalog_size = 0;
 	
-	catalog = open_directory(path_dir);
+	catalog = open_directory(path_dir, &catalog_size);
 		
 	if(catalog == NULL)
 		exit(EXIT_FAILURE);
 	
-	for(int i = 0; i < sizeof(catalog); i++)
+	for(int i = 0; i < catalog_size; i++){
 		printf("%s\n", catalog[i]);
+	}
+	free_memory(catalog, catalog_size);
 }
 
-char** open_directory(char* path_dir){
+char** open_directory(char* path_dir, size_t* size){
 
-	char** catalog ;	
-	
+	char** catalog;	
 	DIR *dir;
 	struct dirent *ent;
 
 	if((dir = opendir(path_dir)) != NULL){
 		catalog = (char**)malloc(sizeof(char*));
-		size_t size = 1;
+		*size += 1;
 		
 		if(catalog == NULL){
-			puts("Failed allocate memory");
-			return NULL;
+				puts("Failed allocate memory");
+				return NULL;
 		}
-		
 		for(int i = 0; (ent = readdir(dir)) != NULL; i++){
-			if(size == i)
-				catalog = realloc(catalog, size * 2);
-			
-			catalog[i] = ent->d_name;
+			if(*size <= i){
+				catalog = realloc(catalog, ++(*size) * sizeof(char*) );
+				
+				if(catalog == NULL){
+					puts("Failed allocate memory");
+					return NULL;
+				}
+			}
+			catalog[i] = name_file_or_dir(path_dir, ent->d_name);
 		}
 		closedir(dir);
 	}
@@ -58,4 +65,36 @@ char** open_directory(char* path_dir){
 		return NULL;
 	}
 	return catalog;
+}
+
+char* name_file_or_dir(char* path_dir, char* name)
+{
+	struct stat buf;
+	char* file_info = (char*)malloc( (strlen(name)  + 20) * sizeof(char));
+	{
+	 	char path_to_file[strlen(path_dir) + strlen(name) + 1];
+		sprintf(path_to_file, "%s/%s", path_dir, name);
+		
+		lstat(path_to_file, &buf);
+	}
+
+	if(S_ISREG(buf.st_mode)){
+		sprintf(file_info, "f %s %ld", name, buf.st_size);
+	}
+	else if(S_ISDIR(buf.st_mode)){
+		sprintf(file_info, "d %s", name);
+	}
+	else if(S_ISLNK(buf.st_mode)){
+		sprintf(file_info, "error %s", name);
+	}
+	
+	return file_info;
+}
+
+void free_memory(char** arr,int size){
+
+	for(int i = 0; i < size; i++){
+		free(arr[i]);
+	}	
+	free(arr);
 }
